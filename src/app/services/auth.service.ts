fcc41f71f6a113f69c70a64f8a29f87e74c8dcf5
luismiguel010 <luismiguel010@gmail.com>
2021-10-16 14:26:19 -0500

    Se agregan métodos de autenticación

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,18 +3,23 @@ import { AuthenticationResponse } from './../models/AuthenticationResponse';
 import { Injectable } from '@angular/core';
 import { Admin } from '../models/Admin';
 import { Observable } from 'rxjs';
+import { sha256 } from 'js-sha256';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private authenticateResponse: AuthenticationResponse;
+  private authenticationResponse: AuthenticationResponse;
+  private authenticationRequest: AuthenticationRequest;
   private admin!: Admin;
   private token!: string;
 
-  constructor() {
-    this.authenticateResponse = new AuthenticationResponse();
+  constructor(private http: HttpClient) {
+    this.authenticationResponse = new AuthenticationResponse();
+    this.authenticationRequest = new AuthenticationRequest();
   }
 
   public getAdmin(): Admin {
@@ -27,14 +32,34 @@ export class AuthService {
     return new Admin();
   }
 
-  public getToken(): string {
+  public getToken(): any {
     if (this.token != null) {
       return this.token;
     } else if (this.token == null && sessionStorage.getItem('token') != null) {
       this.token = sessionStorage.getItem('token') || '{}';
       return this.token;
+    } else {
+      return null;
     }
-    return '';
+  }
+
+  login(authenticationRequest: AuthenticationRequest): Observable<AuthenticationResponse> {
+    this.authenticationRequest.username = authenticationRequest.username;
+    this.authenticationRequest.password = sha256(authenticationRequest.password);
+    return this.http.post<AuthenticationResponse>(environment.url_backend, this.authenticationRequest);
+  }
+
+  saveToken(accessToken: string): void {
+    this.token = accessToken;
+    sessionStorage.setItem('token', accessToken);
+  }
+
+  getDataToken(accessToken: string): any {
+    if (accessToken != null) {
+      return JSON.parse(atob(accessToken.split(".")[1]));
+    } else {
+      return null;
+    }
   }
 
 
