596e04c7e85ccea442000e5a2f757d32891d566f
luismiguel010 <luismiguel010@gmail.com>
2022-02-17 22:12:33 -0500

    Ofertas laborales en crear y en vista actualizada

--- src/app/modals/job-create-modal/job-create-modal.component.ts
+++ src/app/modals/job-create-modal/job-create-modal.component.ts
@@ -4,6 +4,7 @@ import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 import { JobService } from 'src/app/services/job.service';
 import swal from 'sweetalert2';
 import { v4 as uuid } from 'uuid';
+import { Professions } from 'src/app/enums/professions';
 
 @Component({
   selector: 'app-job-create-modal',
@@ -13,10 +14,17 @@ import { v4 as uuid } from 'uuid';
 export class JobCreateModalComponent implements OnInit {
 
   job: Job = new Job();
+  category_array: string[] = new Array();
+  newCategory!: string;
 
   constructor(public modalRef: MdbModalRef<JobCreateModalComponent>, private jobService: JobService) { }
 
   ngOnInit(): void {
+    this.category_array = Object.values(Professions);
+  }
+
+  addCategory(newCategory: string): void {
+    this.category_array.push(newCategory);
   }
 
   close(): void {
@@ -29,12 +37,35 @@ export class JobCreateModalComponent implements OnInit {
 
   public send(): void {
     if (this.job.name == null) {
-      swal.fire('Error al actualizar oferta', 'Campo del nombre vacío', 'error')
+      swal.fire('Error al crear oferta', 'Campo del nombre vacío', 'error')
       return;
     }
     if (this.job.description == null) {
-      swal.fire('Error al actualizar oferta', 'Campo de descripción vacío', 'error')
+      swal.fire('Error al crear oferta', 'Campo de descripción vacío', 'error')
       return;
+    }
+    if (this.job.category == null) {
+      swal.fire('Error al crear oferta', 'Campo de categoría vacío', 'error')
+    }
+    if (this.job.category == 'Otro') {
+      this.job.category = this.newCategory;
+    }
+    if (this.job.dateInit == null) {
+      this.job.dateInit = new Date()
+    }
+    if (this.job.dateFinish == null) {
+      let date: Date = new Date()
+      date.setDate(date.getDate() + 90)
+      this.job.dateFinish = date
+    }
+    if (this.job.salary == null) {
+      this.job.salary = "No se especifica"
+    }
+    if (this.job.phone == null) {
+      this.job.phone = "No se especifica"
+    }
+    if (this.job.company == null) {
+      this.job.company = "No se especifica"
     }
     this.job.idJob = uuid().toString();
     this.job.state = true;
